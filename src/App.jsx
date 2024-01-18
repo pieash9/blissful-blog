8dd4831b7bfe4f297ba3dca72a4803da76682445
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-01-18 12:19:14 +0600

    create the header section

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+import Projects from "./pages/Projects";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
