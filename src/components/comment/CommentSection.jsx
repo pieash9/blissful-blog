04fed8d6f88590d299a1fc655b04d4ddabedeba1
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-01-24 20:08:43 +0600

    add like functionality to the comment component

--- src/components/comment/CommentSection.jsx
+++ src/components/comment/CommentSection.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Alert, Button, Textarea } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Comment from "./Comment";
 
 const CommentSection = ({ postId }) => {
@@ -11,8 +11,8 @@ const CommentSection = ({ postId }) => {
   const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
   const [comments, setComments] = useState([]);
+  const navigate = useNavigate();
 
-  console.log(comments);
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (comment.length > 200) {
@@ -51,6 +51,36 @@ const CommentSection = ({ postId }) => {
     };
     getComments();
   }, []);
+
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await axios.put(
+        `${import.meta.env.VITE_URL}/comment/likeComment/${commentId}`,
+        {},
+        { withCredentials: true }
+      );
+      if (res.data) {
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: res.data.likes,
+                  numberOfLikes: res.data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -114,7 +144,7 @@ const CommentSection = ({ postId }) => {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} />
+            <Comment key={comment?._id} comment={comment} onLike={handleLike} />
           ))}
         </>
       )}
