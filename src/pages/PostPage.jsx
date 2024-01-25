8f586db2af1bc21e8d036c9c77084c5fc0dd998f
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-01-25 09:19:24 +0600

    added recent article section

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -3,12 +3,14 @@ import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CommentSection from "../components/comment/CommentSection";
+import PostCard from "../components/card/PostCard";
 
 const PostPage = () => {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState();
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -33,6 +35,23 @@ const PostPage = () => {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      (async function () {
+        const res = await axios.get(
+          `${import.meta.env.VITE_URL}/post/getPosts?limit=3`
+        );
+
+        if (res.data) {
+          setRecentPosts(res.data.posts);
+        }
+      })();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
   if (loading)
     return (
       <div className="flex justify-center items-center min-h-screen">
@@ -68,6 +87,14 @@ const PostPage = () => {
       ></div>
 
       <CommentSection postId={post._id} />
+
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5 font-semibold">Recent Articles</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+      </div>
     </main>
   );
 };
