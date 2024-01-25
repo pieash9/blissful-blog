b2d2f019a1b495c85b9a594d118feb26aeb0ca39
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-01-25 18:56:34 +0600

    fix deploy issues

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000/api/v1",
-        secure: false,
-      },
+  build: {
+    rollupOptions: {
+      external: ["react-icons/fa"],
     },
   },
   plugins: [react()],
