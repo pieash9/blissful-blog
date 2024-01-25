c67549ccbb6ed63680d3b899b69c7f4ad87d963f
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-01-25 19:18:57 +0600

    fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["react-icons/fa"],
-    },
-  },
   plugins: [react()],
 });
